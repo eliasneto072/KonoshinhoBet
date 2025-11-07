6e806d8783a9f6f1cd507b13f6c44c8f0038d098
Ian <ianjudo552@gmail.com>
2025-11-07 04:50:54 -0300

    footer konoshi

--- konoshisbet/src/components/Footer.tsx
+++ konoshisbet/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer" id="contact">
-      <p>© 2025 Supreme Casino | Todos os direitos reservados.</p>
+      <p>© 2025 Konoshi's Casino | Todos os direitos reservados.</p>
       <p>Proibido para menores de 18 anos 🎲</p>
     </footer>
   );
